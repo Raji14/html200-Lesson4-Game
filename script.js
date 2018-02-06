cd4f3959ceab890ac0d5bef903e4203cbf6f98c1
Rajitha <rajitha.rmn@gmail.com>
2018-02-05 23:29:44 -0800

    added comments, edited stylesheet

--- script.js
+++ script.js
@@ -29,58 +29,63 @@ function createMap(width, height, canvas, startPos, treasurePos, adventureLogger
       }
       this.currentPos = newPos;
       this.adventureLogger.log("Moving " + direction + ".");
+
+      //play sound on move
       document.getElementById("moveSound").currentTime = 0;
       document.getElementById("moveSound").play();
 
-
+      //when treasure is found, the player wins
       if (this.currentPos.x == this.treasurePos.x && this.currentPos.y == this.treasurePos.y) {
         this.treasureFound = true;
         this.adventureLogger.log("Yay! You found the treasure!")
 
+        //disable the move controls when treasure is found
+        document.getElementById("moveControls").classList.add("disabled");
       }
       this.draw();
-
     },
+
+    //Move east
     moveEast: function () {
       const oldPos = this.currentPos;
       const newPos = makePos(oldPos.x + 1, oldPos.y);
       this.move("east", newPos);
-
     },
+
+    //Move west
     moveWest: function () {
       const oldPos = this.currentPos;
       const newPos = makePos(oldPos.x - 1, oldPos.y);
       this.move("west", newPos)
     },
+
+    //Move North
     moveNorth: function () {
       const oldPos = this.currentPos;
       const newPos = makePos(oldPos.x, oldPos.y + 1);
       this.move("north", newPos)
     },
+
+    //Move South
     moveSouth: function () {
       const oldPos = this.currentPos;
       const newPos = makePos(oldPos.x, oldPos.y - 1);
       this.move("south", newPos)
     },
 
+    //Check if position is inbound
     inBounds: function (pos) {
       return pos.x >= 0 && pos.x < this.width && pos.y >= 0 && pos.y < this.height;
     },
 
-    currentPosition: function () {
-      return this.currentPos;
-    },
-
-    treasurePosition: function () {
-      return this.treasurePos;
-    },
-
+    // Draws the map
     draw: function () {
       var ctx = this.canvas.getContext("2d");
       ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       ctx.strokeStyle = "#333";
       ctx.lineWidth = 1;
 
+      //Defines the width
       const cellSize = this.canvas.width / this.width;
       const gridSize = width * cellSize
 
@@ -96,28 +101,31 @@ function createMap(width, height, canvas, startPos, treasurePos, adventureLogger
         ctx.lineTo(gridSize, j);
         ctx.stroke();
       }
-      //draw user
 
-      //inverse the axis to start from the bottom left
-      this.drawSquare(ctx, this.currentPos.x, this.currentPos.y);
+      //draw the player at current position
+      this.drawPlayer(ctx, this.currentPos.x, this.currentPos.y);
 
-      //display treasure icon
+      //when treasure is found, proclaim victory!
       if (this.treasureFound) {
         const x = this.treasurePos.x * cellSize;
         const y = (-(this.treasurePos.y - this.height) - 1) * cellSize;
-
+        //display treasure icon
         var img = new Image();
         img.src = 'images/if_treasure_45274.png';
         img.addEventListener('load', function () {
           ctx.drawImage(img, x, y)
         }, false);
-
+        //display victory message
+        ctx.font = "30px Arial";
+        ctx.fillText("Yay, you found the treasure!", 10, 50);
       }
     },
 
-    drawSquare: function (ctx, x, y) {
+    //draw the player at a given position
+    drawPlayer: function (ctx, x, y) {
       ctx.beginPath();
       const cellSize = this.canvas.width / this.width;
+      //inverse the axis to start from the bottom left
       ctx.rect(x * cellSize, (-(y - this.height) - 1) * cellSize, cellSize, cellSize);
       ctx.fillStyle = "crimson";
       ctx.fill();
@@ -127,6 +135,7 @@ function createMap(width, height, canvas, startPos, treasurePos, adventureLogger
   return map;
 }
 
+//
 function makePos(x, y) {
   return {
     x: x,
@@ -134,13 +143,14 @@ function makePos(x, y) {
   }
 }
 
-// generate a random tresaure position
+// generate a random treasure position
 function generateRandomTreasurePos(width, height) {
   const x = Math.floor(Math.random() * width);
   const y = Math.floor(Math.random() * height);
   return makePos(x, y);
 }
 
+//generate the console logger
 function createAdventureLogger() {
   return {
     logContainer: document.getElementById("log"),
@@ -175,12 +185,15 @@ function createAdventureLogger() {
 // TODO: remove when done with testing
 startGame();
 
+
 var adventureLogger = createAdventureLogger();
 var canvas = document.getElementById("myCanvas");
 const width = 5;
 const height = 5;
-var map = createMap(5, 5, canvas, makePos(0, 0), generateRandomTreasurePos(width, height), adventureLogger);
+var map = createMap(width, height, canvas, makePos(0, 0), generateRandomTreasurePos(width, height), adventureLogger);
 map.draw();
+
+//set up move controls
 document.getElementById("imgEast").addEventListener("click", function () {
   map.moveEast();
 });
@@ -214,13 +227,3 @@ document.getElementById("imgSouth").addEventListener("click", function () {
 // TODO: Use the player's name in the adventure log and in the display
 // TODO: Log a welcome message with directions (how many moves you get, etc)
 // TODO: Log a victory or defeat message
-
-//// Logger testing
-//adventureLogger.clear();
-//adventureLogger.log("My first log><&");
-//var i = 0;
-//while (i < 50) {
-//  adventureLogger.log("some other message " + i);
-//  i++;
-//}
-//adventureLogger.log("my last log");
